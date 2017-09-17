ff943d62ba2d9d22884795141c1320350b2db903
Taher Dhilawala <taher435@gmail.com>
2017-09-17 21:14:28 +0800

    Fix code

--- web/models/application.ex
+++ web/models/application.ex
@@ -2,7 +2,7 @@ defmodule Parma.Application do
   use Parma.Web, :model
 
   @primary_key {:id, :binary_id, autogenerate: true}
-  @foreign_key_type :binary_id
+
   schema "applications" do
     field :name, :string
 
@@ -10,15 +10,14 @@ defmodule Parma.Application do
   end
 
   @required_fields ~w(name)a
-  @optional_fields ~w()a
 
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:name])
-    |> unique_constraint(:name)
+    |> cast(params, @required_fields)
     |> validate_required(@required_fields)
+    |> unique_constraint([:name])    
   end
 end
